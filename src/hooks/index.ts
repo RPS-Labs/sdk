0fdc3b1b78d4be5bb5d67c2a54b33b8e277dc168
gabrielantonyxaviour <gabrielantonyxaviour.24ec@licet.ac.in>
2023-11-16 17:26:30 +0530

    add index

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,1 +1,7 @@
 export { useLeaderBoard } from "./useLeaderBoard";
+export { useTicketPrice } from "./useTicketPrice";
+export { useTrades } from "./useTrades";
+export { useWinners } from "./useWinners";
+export { usePot } from "./usePot";
+export { usePotById } from "./usePotById";
+export { useUserTickets } from "./useUserTickets";
