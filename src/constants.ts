d83e4d63fbddc369c548b2d6cb9cff4266fcd547
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2023-11-23 10:56:34 +0530

    add swr

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,15 @@
+import axios, { AxiosResponse } from "axios";
+
 export const API_URL = "https://api.rpslabs.io";
+export const fetcher: (endpoint: string) => Promise<any> = async (endpoint) => {
+  let responseData = null;
+  try {
+    const response: AxiosResponse<any> = await axios.get(endpoint);
+    if (response.data.code != 200) {
+      throw new Error(response.data.message);
+    }
+  } catch (error: any) {
+    return error.message || { message: "An error occurred", code: 500 };
+  }
+  return responseData;
+};
--- src/hooks/useWinners.ts
+++ src/hooks/useWinners.ts
@@ -1,117 +1,29 @@
-import { useState, useEffect } from "react";
-import axios, { AxiosResponse } from "axios";
-import { API_URL } from "../constants";
+import { API_URL, fetcher } from "../constants";
+import useSWR from "swr";
 
 interface WinnersProps {
   chain: "ethereum" | "linea" | "sepolia" | "taiko" | "mantle" | "scroll";
-  onSuccess: (data: ApiData) => void;
-  onError: (error: ApiError) => void;
   // Add more parameters as needed
 }
 
-interface ApiData {
-  code: number;
-  message: string;
-}
-
-interface ApiError {
-  // Customize this based on the actual structure of your error object
-  message: string;
-  code: number;
-  // Add other error properties as needed
-}
-
 interface ApiHookResult {
-  data: ApiData | null;
-  error: ApiError | null;
-  isIdle: boolean;
+  data: any;
+  error: any;
   isLoading: boolean;
-  isSuccess: boolean;
-  isError: boolean;
-  isRefetching: boolean;
-  refetch: (options: {
-    throwOnError: boolean;
-    cancelRefetch: boolean;
-  }) => Promise<ApiData | null>; // Adjust the Promise type based on your actual return data
-  status: "idle" | "error" | "loading" | "success";
+  isValidating: boolean;
+  mutate: (data: any, options: any) => {};
 }
 
-// Define your custom hook
-export function useWinners({
-  chain,
-  onSuccess,
-  onError,
-}: WinnersProps): ApiHookResult {
-  const [data, setData] = useState<ApiData | null>(null);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [isSuccess, setIsSuccess] = useState<boolean>(false);
-  const [isError, setIsError] = useState<boolean>(false);
-  const [isRefetching, setIsRefetching] = useState<boolean>(false);
-  const [error, setError] = useState<ApiError | null>(null);
-  const [status, setStatus] = useState<
-    "idle" | "error" | "loading" | "success"
-  >("idle");
-  const [isIdle, setIsIdle] = useState<boolean>(true);
-
-  const fetchData: () => Promise<ApiData | null> = async () => {
-    let responseData = null;
-    setStatus("loading");
-    setIsIdle(false);
-    try {
-      const response: AxiosResponse<ApiData> = await axios.get(
-        `${API_URL}/pot/winners?chain=${chain}`
-      );
-      if (response.data.code == null || response.data.code == 200) {
-        setData(response.data);
-        onSuccess(response.data);
-        setIsSuccess(true);
-        setStatus("success");
-        responseData = response.data;
-      } else {
-        setIsError(true);
-        setError(response.data);
-        onError(response.data);
-        setStatus("error");
-      }
-    } catch (error: any) {
-      setIsError(true);
-      setError(error.message || { message: "An error occurred", code: 500 });
-      setStatus("error");
-    } finally {
-      setIsLoading(false);
-    }
-    setIsIdle(true);
-    return responseData;
-  };
-  useEffect(() => {
-    fetchData();
-  }, [chain, onSuccess, onError]);
-
-  async function refetch(options: {
-    throwOnError: boolean;
-    cancelRefetch: boolean;
-  }): Promise<ApiData | null> {
-    if (options.cancelRefetch) return null;
-    try {
-      setIsRefetching(true);
-      const data = await fetchData();
-      setIsRefetching(false);
-      return data;
-    } catch (err) {
-      if (options.throwOnError) throw err;
-    }
-    return null;
-  }
-
+export function useWinners({ chain }: WinnersProps): ApiHookResult {
+  const { data, error, isLoading, isValidating, mutate } = useSWR(
+    `${API_URL}/pot/winners?chain=${chain}`,
+    () => fetcher(`${API_URL}/pot/winners?chain=${chain}`)
+  );
   return {
     data,
-    isError,
-    isLoading,
-    isSuccess,
     error,
-    refetch,
-    isRefetching,
-    isIdle,
-    status,
+    isLoading,
+    isValidating,
+    mutate,
   };
 }
